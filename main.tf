6807b3a69f7b04b08e9a80ba56ebac8624278447
Charleen Tan <ctan@aviatrix.com>
2025-02-03 17:54:15 +0000

    update

--- main.tf
+++ main.tf
@@ -20,12 +20,12 @@ module "controller_build" {
   availability_zone         = var.availability_zone
   vpc_cidr                  = var.controlplane_vpc_cidr
   subnet_cidr               = var.controlplane_subnet_cidr
-  environment               = var.environment                      #For internal use only
-  ami_id                    = var.controller_ami_id                #For internal use only
-  use_existing_keypair      = var.controller_use_existing_keypair  #For internal use only
-  key_pair_name             = var.controller_key_pair_name         #For internal use only
-  registry_auth_token       = var.registry_auth_token              #For internal use only
-  additional_bootstrap_args = var.additional_bootstrap_args        #For internal use only
+  environment               = var.environment                     #For internal use only
+  ami_id                    = var.controller_ami_id               #For internal use only
+  use_existing_keypair      = var.controller_use_existing_keypair #For internal use only
+  key_pair_name             = var.controller_key_pair_name        #For internal use only
+  registry_auth_token       = var.registry_auth_token             #For internal use only
+  additional_bootstrap_args = var.additional_bootstrap_args       #For internal use only
 
   depends_on = [
     module.iam_roles
