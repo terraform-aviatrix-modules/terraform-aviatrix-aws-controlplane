8bac5cb080b0bcb8adb179aa0727b187105d6773
jlin880 <jlin@aviatrix.com>
2025-03-07 14:28:14 -0800

    update tags for tracking containerized controller deployed in CI

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "controller_build" {
   key_pair_name             = var.controller_key_pair_name        #For internal use only
   registry_auth_token       = var.registry_auth_token             #For internal use only
   additional_bootstrap_args = var.additional_bootstrap_args       #For internal use only
-
+  tags                      = var.tags
   depends_on = [
     module.iam_roles
   ]
@@ -69,7 +69,7 @@ module "copilot_build" {
   environment              = var.environment                  #For internal use only
   use_existing_keypair     = var.copilot_use_existing_keypair #For internal use only
   key_pair_name            = var.copilot_key_pair_name        #For internal use only
-
+  tags                     = var.tags
   allowed_cidrs = {
     "tcp_cidrs" = {
       protocol = "Tcp"
